09fdc25a4c867cef0fd465234df9a638f6e59b32
Anuraj Taya <anurajtaya@gmail.com>
2022-02-16 23:56:03 +0000

    Added pin functionality
    Added function that enables taking pins.
    Also added validation for pins.

--- proto.py
+++ proto.py
@@ -8,16 +8,21 @@ class PasswordRetriever():
     def __init__ (self):
         
         self.passwords = []
+        self.pins = []
         self.num_enteries = len(self.passwords)
         self.window = tkinter.Tk()
-        self.min_len = 8
+        self.min_tap_len = 8
+        self.pin_len = 4
     # Function to clear the text in the entry field    
-    def clear_text(self, event):
+    def clear_tp(self, event):
         self.password_field.delete(0, END)
     
+    def clear_pin(self, event):
+        self.pin_field.delete(0, END)
+
     # Runs the main tk window
     def tap_passwords(self):
-        self.window.title("GUI")
+        self.window.title("TAP PASSWORDS")
         self.window.geometry("1000x700")
         img = PhotoImage(file="./bg.png")
         label = Label(
@@ -27,9 +32,10 @@ class PasswordRetriever():
         label.place(x=0, y=0,relwidth=1, relheight=1)
         self.password_field = tkinter.Entry(self.window, show="\u2022", font=("Helvetica, 28"), background='#BDBDBD', bd=0, justify='center') #second input-field is placed on position 11 (row - 1 and column - 1)
         self.password_field.place(relx=0.5, rely=0.40, anchor=CENTER)
-        self.password_field.bind("<Return>", self.validation)
-        self.password_field.bind("<BackSpace>", self.clear_text)
+        self.password_field.bind("<Return>", self.tap_password_validation)
+        self.password_field.bind("<BackSpace>", self.clear_tp)
         self.window.mainloop()
+
     # Function to get the list of passwords
     def get_passwords(self):
         return self.passwords
@@ -43,7 +49,7 @@ class PasswordRetriever():
         else:
             try:
                 # If the password is smaller than the min length then report
-                if len(password)<self.min_len:
+                if len(password)<self.min_tap_len:
                     msg = 'Password to short.'
                     self.password_field.delete(0, END)
                 # If not then append the password to the list and show success message
@@ -55,7 +61,49 @@ class PasswordRetriever():
                 tkinter.messagebox.showerror('error', ep)
         tkinter.messagebox.showinfo('message', msg)
 
+    def pins_passwords(self):
+        self.window = tkinter.Tk()
+        self.window.title("PIN PASSWORDS")
+        self.window.geometry("1000x700")
+        img = PhotoImage(file="./bg.png")
+        label = Label(
+            self.window,
+            image=img
+        )
+        label.place(x=0, y=0,relwidth=1, relheight=1)
+        # label2 = Label(self.window, "Tap Passwords", font=("Helvetica, 28"), background='#BDBDBD', bd=0, justify='center')
+        self.pin_field = tkinter.Entry(self.window, show="\u2022", font=("Helvetica, 28"), background='#BDBDBD', bd=0, justify='center') #second input-field is placed on position 11 (row - 1 and column - 1)
+        self.pin_field.place(relx=0.5, rely=0.40, anchor=CENTER)
+        self.pin_field.bind("<Return>", self.pin_validation)
+        self.pin_field.bind("<BackSpace>", self.clear_pin)
+        self.window.mainloop()
+
+    # Validates the password with respect to its length.
+    def pin_validation(self, event):
+        pin = self.pin_field.get()
+        msg = ''
+        if len(pin)==0:
+            msg = 'Password can\'t be empty'
+        else:
+            try:
+                if not pin.isdigit():
+                    msg = 'It should be a digit.' 
+                elif len(pin)!=4:
+                    msg = 'Has to be of length 4.'
+                else:
+                    self.pins.append(pin)
+                    self.pin_field.delete(0, END)
+                    msg = 'Success!'
+            except Exception as ep:
+                tkinter.messagebox.showerror('error', ep)
+        tkinter.messagebox.showinfo('message', msg)
+
+    def get_pins(self):
+        return self.pins
+
 
 pr = PasswordRetriever()
 pr.tap_passwords()
 print(pr.get_passwords())
+pr.pins_passwords()
+print(pr.get_pins())
