f55114b4aadca1d0db9092dfed84fbc03ff52845
Vrinda Sharma <2439103s@student.gla.ac.uk>
2022-02-22 22:22:34 +0000

    Completed testing and validating functionality

--- visual_cue_proto.py
+++ visual_cue_proto.py
@@ -8,17 +8,24 @@ class PasswordRetriever():
     def __init__ (self):
         
         self.passwords = []
+        self.set_passwords = []
+        self.set_password = None
+        self.password = None
         self.num_enteries = len(self.passwords)
         self.window = tkinter.Tk()
         self.min_tap_len = 8
+        self.count = 0
+        self.attempt = 0
         self.label_box_1 = tkinter.Label()
         self.label_box_2 = tkinter.Label()
         self.label_box_3 = tkinter.Label()
         self.label_box_4 = tkinter.Label()
+        self.command_label = tkinter.Label()
+    
     # Function to clear the text in the entry field    
     def clear_tp(self, event):
         self.password_field.delete(0, END)
-
+        
     # Runs the main tk window
     def tap_passwords(self):
 
@@ -60,17 +67,26 @@ class PasswordRetriever():
         self.label_box_3.place(relx=0.55, rely=0.70, anchor=CENTER)
         self.label_box_4 = tk.Label(self.window, background='#BDBDBD', width=5, height=2)
         self.label_box_4.place(relx=0.65, rely=0.70, anchor=CENTER)
-        string_var = StringVar()
-        string_var.trace("w", lambda name, index, mode, sv=string_var: callback(string_var))
-        self.password_field = tkinter.Entry(self.window, show="\u2022", font=("Helvetica, 28"), background='#BDBDBD', bd=0, justify='center', textvariable=string_var) #second input-field is placed on position 11 (row - 1 and column - 1)
-        self.password_field.place(relx=0.5, rely=0.40, anchor=CENTER)
-        self.password_field.bind("<Return>", self.tap_password_validation)
-        self.password_field.bind("<BackSpace>", self.clear_tp)
-        self.window.mainloop()
+        self.command_label = tkinter.Label(self.window, font=("Helvetica, 20"), background='#BDBDBD', bd=0)
+        self.command_label.place(relx=0.5, rely=0.2, anchor=CENTER)
+        
+        # Function to enter the password
+        def enter_password():
+            string_var = StringVar()
+            string_var.trace("w", lambda name, index, mode, sv=string_var: callback(string_var))
+            self.password_field = tkinter.Entry(self.window, show="\u2022", font=("Helvetica, 28"), background='#BDBDBD', bd=0, justify='center', textvariable=string_var) #second input-field is placed on position 11 (row - 1 and column - 1)
+            self.password_field.place(relx=0.5, rely=0.40, anchor=CENTER)
+            self.password_field.bind("<Return>", self.processing_password)
+            self.password_field.bind("<BackSpace>", self.clear_tp)
 
-    # Function to get the list of passwords
-    def get_passwords(self):
-        return self.passwords
+        if(self.password is not None):
+            self.command_label.config(text='Enter your password')
+            enter_password()
+        else:
+            self.command_label.config(text='Set your password')
+            enter_password()
+        
+        self.window.mainloop()
     
     # Validates the characters of the password
     def tap_password_char_check(self, password):
@@ -79,8 +95,7 @@ class PasswordRetriever():
         return all(matched_char_list)
 
     # Validates the password with respect to its length.
-    def tap_password_validation(self, event):
-        password = self.password_field.get()
+    def tap_password_validation(self, password):
         msg = ''
         if len(password)==0:
             msg = 'Password can\'t be empty'
@@ -93,17 +108,70 @@ class PasswordRetriever():
                 # If not then append the password to the list and show success message
                 else:
                     if self.tap_password_char_check(password):
-                        self.passwords.append(password)
                         self.password_field.delete(0, END)
                         msg = 'Success!'
+                        tkinter.messagebox.showinfo('message', msg)
+                        return True
                     else:
                         msg = 'Password characters incorrect. Only Tap the specified keys.'
                         self.password_field.delete(0, END)
             except Exception as ep:
                 tkinter.messagebox.showerror('error', ep)
         tkinter.messagebox.showinfo('message', msg)
+        return False
 
+    # Function to confirm password
+    def confirm_password_message(self):
+        return tk.messagebox.askyesno(title='Confirmation',message='Are you satisfied with the password?')
+
+    # Function to allow user to consecutively enter password 5 times
+    def confirm_password(self, password):
+        if(self.count == 4):
+            msg = "PASSWORD CONFIRMED! 5 consecutive attempts reached."
+            tkinter.messagebox.showinfo('message', msg)
+            self.password = self.set_password
+            tk.messagebox.showinfo("Instructions", "You are going to enter the created tap password 6 times\n\nOnces entered you will not have a chance to change it\n\nA count for number of attempts left will be displayed after every entry\n\nGood Luck, press Ok to start test.")
+        else:
+            if (self.tap_password_validation(password)):
+                if (password == self.set_password):
+                    self.count += 1
+                    msg = 'CORRECT! Consecutive count = '+str(self.count) 
+                else:
+                    msg = 'INCORRECT! Consecutive count RESET = '+str(self.count)      
+            tkinter.messagebox.showinfo('message', msg)
+
+    # Function to check the password for correctness
+    def check_password(self, password):
+        return self.password == password
+
+    # Function for processing the password
+    def processing_password(self, event):
+        password = self.password_field.get()
+        
+        if(self.set_password is None):
+            if (self.tap_password_validation(password) and (self.confirm_password_message())):
+                msg = 'Success!\nNOW enter the correct password 5 consecutive time to confirm the password.'
+                tkinter.messagebox.showinfo('message', msg)
+                self.set_password = password
+                self.set_passwords.append(password)
+        elif(self.password is None):
+                self.confirm_password(password)
+                self.set_passwords.append(password)
+        else:
+            if(self.check_password(password)):
+                self.attempt += 1
+                msg = "Password is correct."
+            elif(not self.check_password(password) and self.attempt<6):
+                self.attempt += 1
+                msg = 'Password is incorrect. Attempts left = '+ str(6 - self.attempt)
+            else:
+                msg = 'Sorry! You are out of attempts.'
+            tkinter.messagebox.showinfo('message', msg)
+            self.passwords.append(password)
+        tk.messagebox.showinfo("Thank you for participating!")
+        self.tap_passwords()
 
 pr = PasswordRetriever()
 pr.tap_passwords()
 print(pr.get_passwords())
+print(pr.set_passwords)
